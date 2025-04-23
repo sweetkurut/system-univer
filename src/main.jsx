4e46cf0240065942abe15926fd36dee27e770e95
Nursultan <esenov.nursultan22@gmail.com>
2025-04-23 23:05:33 +0600

    add board page

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/Login.jsx";
 import Deals from "./pages/Deals/Deals.jsx";
 import Report from "./pages/Report/Report.jsx";
+import BoardPage from "./pages/Board/BoardPage.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")).render(
                 <Route path="/" element={<Login />} />
                 <Route path="/calendar" element={<Deals />} />
                 <Route path="/report" element={<Report />} />
+                <Route path="/board" element={<BoardPage />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>
